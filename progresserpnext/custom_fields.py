7765483b5c71f94a2f0bc26cf6e6a8472fa048f5
Matthias Ludwig <ludwig@progress-psd.com>
2025-02-27 16:15:28 +0100

    ludmat/[erpnext]changing custom fields
    * change custom fields
    
    * delete custom field

--- progresserpnext/custom_fields.py
+++ progresserpnext/custom_fields.py
@@ -30,7 +30,7 @@ def get_custom_fields():
 				"search_index": 1,
 			}
 		],
-		("Quotation Item", "Sales Order Item", "Sales Invoice Item", "Delivery Note Item"): [
+		("Quotation Item", "Sales Order Item", "Sales Invoice Item"): [
 			{
 				"fieldname": "custom_parent_line_idx",
 				"label": _("Parent Line Index"),
@@ -57,6 +57,40 @@ def get_custom_fields():
 				"fieldtype": "Data",
 			},
 		],
+		"Delivery Note Item": [
+			{
+				"fieldname": "custom_parent_line_idx",
+				"label": _("Parent Line Index"),
+				"insert_after": "item_name",
+				"fieldtype": "Int",
+				"non_negative": 1,
+			},
+			{
+				"fieldname": "custom_is_inclusive",
+				"label": _("Is Inclusive"),
+				"insert_after": "custom_parent_line_idx",
+				"fieldtype": "Check",
+			},
+			{
+				"fieldname": "custom_building",
+				"label": _("Building"),
+				"insert_after": "custom_is_inclusive",
+				"fieldtype": "Data",
+			},
+			{
+				"fieldname": "custom_section",
+				"label": _("Section"),
+				"insert_after": "custom_building",
+				"fieldtype": "Data",
+			},
+			{
+				"fieldname": "custom_ahead_delivery_no",
+				"label": _("Ahead Delivery No."),
+				"insert_after": "custom_building",
+				"fieldtype": "Data",
+				"search_index": 1,
+			},
+		],
 		"Sales Order": [
 			{
 				"fieldname": "custom_quotation",
@@ -76,13 +110,6 @@ def get_custom_fields():
 				"fieldtype": "Link",
 				"options": "Sales Order",
 				"in_standard_filter": 1,
-				"search_index": 1,
-			},
-			{
-				"fieldname": "custom_ahead_delivery_no",
-				"label": _("Ahead Delivery No."),
-				"insert_after": "custom_sales_order",
-				"fieldtype": "Data",
 				"search_index": 1,
 			},
 		],
