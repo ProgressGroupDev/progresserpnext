aa15e664336101718016c41a99b26c7886f41c29
noeand1 <noeckler@progress-psd.com>
2025-03-14 14:00:14 +0100

    [erpnext] - delete obsolete custom fields

---
+++ progresserpnext/patches/drop_ahead_delivery_no_in_delivery_note.py
@@ -0,0 +1,12 @@
+import frappe
+from frappe.utils import cstr
+
+def execute():
+    # Delete the custom field
+    frappe.delete_doc_if_exists("Custom Field", "Delivery Note-custom_ahead_delivery_no")
+    
+    # Drop the column from Delivery Note if it exists
+    frappe.db.sql("""
+        ALTER TABLE `tabDelivery Note` 
+        DROP COLUMN IF EXISTS `custom_ahead_delivery_no`;
+    """)
